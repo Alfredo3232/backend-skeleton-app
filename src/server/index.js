afafed8de7730a1a47ea19b23363628f8f3df95f
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-02-02 16:14:57 -0800

    update src structure, use of helmet and mongoose

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,13 @@
+/* eslint-disable no-console */
+// disabled for development
 import express from "express";
+import helmet from "helmet";
+import mongoose from "mongoose";
 
 let app = express();
 const PORT = 3000;
+
+app.use(helmet());
 
 app.get("/", function (req, res) {
     res.send("Hello World");
@@ -10,3 +16,13 @@ app.get("/", function (req, res) {
 app.listen(PORT, () => {
     console.log(`Running at PORT: ${PORT}`);
 });
+
+mongoose
+    .connect("", { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
+    .then(() => {
+        app.listen(PORT || 5000);
+        console.log("Connected MongoDB");
+    })
+    .catch(err => {
+        console.log(err);
+    });

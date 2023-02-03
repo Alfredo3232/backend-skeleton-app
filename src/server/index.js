ad1157af3989d1a1896c18603407d471ed1e5dba
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-02-02 16:35:54 -0800

    add comments

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-// disabled for development
+// disabled for development.
 import express from "express";
 import helmet from "helmet";
 import mongoose from "mongoose";
@@ -17,7 +17,12 @@ app.listen(PORT, () => {
     console.log(`Running at PORT: ${PORT}`);
 });
 
-mongoose
+/*
+    Alfredo3232 - 2/2/2023
+    at least for right now I am thinking of using the MongoDB online service, since it will be more cost effective using different services
+    to handle the work.
+*/
+ mongoose
     .connect("", { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
     .then(() => {
         app.listen(PORT || 5000);

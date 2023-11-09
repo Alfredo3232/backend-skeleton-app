712814d4117b39ced4505c9ee7750d1a026398c3
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2023-11-09 09:03:26 -0800

    Update eslint settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -49,6 +49,7 @@ module.exports = {
             "arrays"    : "never",
             "objects"   : "never",
             "imports"   : "never",
+            "exports"   : "never",
             "functions" : "never"
         }],
         "semi"           : ["error", "always"],
